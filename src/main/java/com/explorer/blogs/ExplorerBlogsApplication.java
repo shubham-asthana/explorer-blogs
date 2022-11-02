a434b51c8291ede3951d76708cb11579b72a3229
Shubham Asthana <shubham.asthana3092@gmail.com>
2022-11-03 01:04:45 +0530

    Initial commit for explorer blogs backend

---
+++ src/main/java/com/explorer/blogs/ExplorerBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.explorer.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorerBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorerBlogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explorer/blogs/ExplorerBlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explorer.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorerBlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
